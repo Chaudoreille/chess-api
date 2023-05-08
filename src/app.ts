6616854812661d329ec99c3217e823033bad8ad9
Chaudoreille <a.chaudoreille@gmail.com>
2023-05-08 16:39:53 +0200

    add route index

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import indexRoutes from './routes/index.routes';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/", indexRoutes);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+export default app;
